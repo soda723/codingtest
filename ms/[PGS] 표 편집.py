721adfb253cb030f8fb6feef4a4a37cb7b515ddf
Minseo Park <80569061+M1nseoPark@users.noreply.github.com>
2022-08-13 23:40:08 +0900

    Update [PGS] 표 편집.py

--- ms/[PGS] 표 편집.py
+++ ms/[PGS] 표 편집.py
@@ -1,9 +1,12 @@
+# 런타임 에러가 나는 이유를 모르겠음
+# 단순 리스트로는 못푸는 문제라는 말을 봐서 절망
 def solution(n, k, cmd):
-    state = [True for _ in range(n)]
-    recmd = []
-    delete = []
+    state = [True for _ in range(n)]   # 표 상태(True가 O)
+    recmd = []  # Z 처리해준 명령어 모음
+    delete = []   # cmd에서 C 나오는 인덱스 위치
     answer = ''
     
+    # Z 나오면 먼저 나온 C 제거
     for i in range(len(cmd)):
         if cmd[i] == "Z":
             recmd.pop(delete[-1])
@@ -13,8 +16,9 @@ def solution(n, k, cmd):
             delete.append(i)
         else:
             recmd.append(cmd[i])
-
-    idx = 0
+    
+    # 명령어 처리
+    idx = 0   # recmd의 인덱스
     while True:
         if idx == len(recmd):
             break
